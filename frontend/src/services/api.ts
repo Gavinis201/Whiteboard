0abb9fe0ed84e45633924d19ae4cc719be08957c
Gavin Smith <gavinjacobsmith@gmail.com>
2025-06-22 16:59:12 -0600

    updated teh api_url and the signalR websocets

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_URL = 'https://bookrata-backend-aadygxdtgbanbefz.westus2-01.azurewebsites.net/api';
-const API_URL = 'http://localhost:5164/api';
+const API_URL = 'https://bookrata-backend-aadygxdtgbanbefz.westus2-01.azurewebsites.net/api';
+// const API_URL = 'http://localhost:5164/api';
 
 export interface Game {
     gameId: number;
