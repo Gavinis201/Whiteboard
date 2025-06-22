ab7dbdfb080486c432a67d85439875ac2152976c
Gavin Smith <gavinjacobsmith@gmail.com>
2025-06-22 17:01:15 -0600

    corrected urls for deployment

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://bookrata-backend-aadygxdtgbanbefz.westus2-01.azurewebsites.net/api';
+const API_URL = 'https://whiteboardv2-backend-ckf7efgxbxbjg0ft.eastus-01.azurewebsites.net/api';
 // const API_URL = 'http://localhost:5164/api';
 
 export interface Game {
