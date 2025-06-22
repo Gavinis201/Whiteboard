7b5196e2fbf3abff0e899d1ae9aca6594f5a5d50
Gavin Smith <gavinjacobsmith@gmail.com>
2025-06-22 17:37:53 -0600

    updated to https signalR

--- frontend/src/services/signalR.ts
+++ frontend/src/services/signalR.ts
@@ -45,7 +45,7 @@ class SignalRService {
             if (this.connection) await this.connection.stop();
 
             this.connection = new HubConnectionBuilder()
-                .withUrl('http://whiteboardv2-backend-ckf7efgxbxbjg0ft.eastus-01.azurewebsites.net/gameHub', {
+                .withUrl('https://whiteboardv2-backend-ckf7efgxbxbjg0ft.eastus-01.azurewebsites.net/gameHub', {
                     transport: HttpTransportType.WebSockets,
                 })
                 .configureLogging(LogLevel.Information)
