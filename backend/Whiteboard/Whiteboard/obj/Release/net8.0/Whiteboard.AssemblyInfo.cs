15fb5fa11e914c7a63384d518a47e3ddc2d4e3c3
Gavin Smith <gavinjacobsmith@gmail.com>
2025-07-09 19:33:43 -0600

    redeployed backend

--- backend/Whiteboard/Whiteboard/obj/Release/net8.0/Whiteboard.AssemblyInfo.cs
+++ backend/Whiteboard/Whiteboard/obj/Release/net8.0/Whiteboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Whiteboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6a966eeb47957b059d817f0474f35e09faa589d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa77fd41e4c6b49ed34a5e3096b374b0ae19674c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Whiteboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Whiteboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
